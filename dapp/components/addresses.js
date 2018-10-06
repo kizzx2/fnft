9f7a2cc8e4bf2101779e05e49569512cdbb8b394
Richard Liang <richardliang2015@u.northwestern.edu>
2018-10-06 10:12:49 -0700

    skeleton 0x order

---
+++ dapp/components/addresses.js
@@ -0,0 +1,18 @@
+export const addressesRopsten = {
+	zeroEx: {
+		AssetProxyOwner: "0xf5fa5b5fed2727a0e44ac67f6772e97977aa358b",
+		ERC20Proxy: "0xb1408f4c245a23c31b98d2c626777d4c0d766caa",
+		ERC721Proxy: "0xe654aac058bfbf9f83fcaee7793311dd82f6ddb4"
+		Exchange: "0x4530c0483a1633c7a1c97d2c53721caff2caaaaf"
+		Forwarder: "0x3983e204b12b3c02fb0638caf2cd406a62e0ead3"
+		OrderValidator: "0x90431a90516ab49af23a0530e04e8c7836e7122f",
+		ZRXToken: "0xff67881f8d12f372d91baae9752eb3631ff0ed00"
+	},
+	setProtocol: {
+		coreAddress: '0x9797d7c3d8d32c01c27476a36c032b7fe32c5b59',
+  		setTokenFactoryAddress: '0x0b24de0bca3e20c53abf4c0f87e24be52324d3a0',
+  		transferProxyAddress: '0x595f8dab94b9c718cbf5c693cd539fd00b286d3d',
+  		vaultAddress: '0x6099e87a16fefe9d778059eeb6d85719fe8a9283',
+	},
+	
+}
